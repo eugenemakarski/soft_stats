ce946e8830f363c0c41a29c926ac8fa09dae9b09
eugene <emakarsk@gmail.com>
2026-05-27 21:51:28 -0400

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
-  # This file should ensure the existence of records required to run the application in every environment (production,
-  # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-  # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-  #
-  # Example:
-  #
-  #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-  #     MovieGenre.find_or_create_by!(name: genre_name)
-  #   end
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+if Rails.env.production? && ActiveRecord::Base.connection.column_exists?(:users, :email)
   User.find_or_create_by(email: ENV["SEED_USER_EMAIL"]) do |u|
     u.password = ENV["SEED_USER_PASSWORD"]
     u.password_confirmation = ENV["SEED_USER_PASSWORD"]
   end
+end
